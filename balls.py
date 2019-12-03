041b40637965be9ccf7594b4be0998d1158ce179
eremeev-2001 <55408905+eremeev-2001@users.noreply.github.com>
2019-12-03 11:17:20 +0300

    Update balls.py
    Начало создания классов и функций

--- balls.py
+++ balls.py
@@ -1,1 +1,22 @@
-import
+import tkinter as tk
+import math
+import random
+from main import*
+
+class Ball():
+    def __init__(self, x, y, dx, dy, life):
+        pass
+    def move():
+        pass
+    def hit():
+        pass
+
+        
+class Ball_1(Ball):
+    pass
+
+class Ball_2(Ball):
+    pass
+
+class Ball_3(Ball):
+    pass
